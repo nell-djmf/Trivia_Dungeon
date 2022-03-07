d6b6e6648f4b0e1938d51a804d9a1496f2e6563b
Nell Flores <nell.djmf@gmail.com>
2022-03-07 15:49:11 -0500

    quiz questions and answers are displaying on gameboard

--- game.js
+++ game.js
@@ -1,22 +1,38 @@
-const DOMAIN = 'https://opentdb.com/api.php?amount=50'
+const DOMAIN = 'https://opentdb.com/api.php?amount=50&type=multiple'
 
-const start = document.querySelector('.start')
-const question = document.getElementsByClassName('question')
+const newEncounter = document.querySelector('.new-encounter')
+const category = document.querySelector('.category')
+const question = document.getElementsByClassName('question')[0]
 const answers = document.getElementsByClassName('answers')
 const progress = document.querySelector('.prog-bar')
 const power = document.querySelector('.powers')
+const ans1 = document.querySelector('.ans1')
+const ans2 = document.querySelector('.ans2')
+const ans3 = document.querySelector('.ans3')
+const ans4 = document.querySelector('.ans4')
 
 
-start.addEventListener('click', async () => {
+
+newEncounter.addEventListener('click', async () => {
     let response = await axios.get(
-        `https://opentdb.com/api.php?amount=50`
+        `https://opentdb.com/api.php?amount=50&type=multiple`
       )
+    let showCategory = response.data.results[0].category
     let showQuestion = response.data.results[0].question
-    let showAnswer = response.data.results[0].correct_answer
-    
-    console.log(showQuestion)
-    console.log(showAnswer)
-    
-  
+    let showAnswer1 = response.data.results[0].correct_answer
+    let showAnswer2 = response.data.results[0].incorrect_answers[0]
+    let showAnswer3 = response.data.results[0].incorrect_answers[1]
+    let showAnswer4 = response.data.results[0].incorrect_answers[2]
+    question.innerHTML = showQuestion
+    ans1.innerHTML = showAnswer1
+    ans2.innerHTML = showAnswer2
+    ans3.innerHTML = showAnswer3
+    ans4.innerHTML = showAnswer4
+    category.innerHTML = showCategory
 
+    console.log(showQuestion)
+    console.log(showAnswer1)
+    console.log(showAnswer2)
+    console.log(response)
+    
 })
